d10d0725c86898fa5a76696b16ec6f6f48832d1e
Diyor <diyorka255@mail.ru>
2026-01-01 19:57:21 +0300

    Коммит №1.1 Убраны лишние импорты для прохождения тестов

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.sleeptracker.functions.*;
 
-import java.time.LocalTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
